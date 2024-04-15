0656d5013122cb06d8cadfa88f270e1038d6ea35
albretsen <bjellanda@gmail.com>
2024-04-15 19:13:29 +0200

    Added external server

--- src/functions/WebVakt-Change-Detection-Function.js
+++ src/functions/WebVakt-Change-Detection-Function.js
@@ -2,22 +2,49 @@
 const { app } = require('@azure/functions');
 const compareWebContent = require('./services/webcompare');
 
+const externalServerURL = 'https://localhost:32774/api/task';
+
 app.http('check', {
     methods: ['POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
         try {
-          context.log(`HTTP function processed request for URL "${request.url}"`);
+            process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+            context.log(`HTTP function processed request for URL "${request.url}"`);
 
             const { WebsiteURL, Monitors } = await request.json();
 
             const changesDetected = await compareWebContent(WebsiteURL, Monitors);
 
+            let response = JSON.stringify({
+                message: "Changes detected successfully.",
+                changes: changesDetected
+            })
+
+            const fetch = (await import('node-fetch')).default;
+
+            const externalResponse = await fetch(externalServerURL, {
+                method: 'POST', 
+                body: response, 
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (!externalResponse.ok) { 
+                context.log(`External server error: Status ${externalResponse.status}`);
+                return {
+                    body: JSON.stringify({
+                        message: "External server error."
+                    }),
+                    status: 500
+                };
+            }
+
+            context.log(`External server responded with OK.`);
             return {
-                body: JSON.stringify({
-                    message: "Changes detected successfully.",
-                    changes: changesDetected
-                })
+                body: response,
+                status: 200 
             };
         } catch (error) {
             context.log(`Error: ${error.message}`);
